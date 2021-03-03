f0e4c5bd4695781908140eb157aa97f569d751eb
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-02 21:39:50 -0800

    added voting get route to group by candidate

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use(require('./candidateRoute'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 
 module.exports = router;
